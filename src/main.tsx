6ddcb0123d2681b8614e0b84ff4fe280d643f253
David Proaño <davisprofranco@hotmail.es>
2024-08-28 09:21:42 -0500

    add theme provider for styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 
 import  store  from './state/store.ts'
 import { Provider } from 'react-redux'
+import { ThemeProvider } from '@/components/core/theme-provider/theme-provider';
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient = new QueryClient()
@@ -11,7 +12,9 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
+      <ThemeProvider>
         <App />
+      </ThemeProvider>
       </Provider> 
     </QueryClientProvider>
 )
