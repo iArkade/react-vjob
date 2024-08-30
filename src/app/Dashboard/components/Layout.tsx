39056732eee63817905191df0bc6372f793348fc
Daniel Velasco <desteban.velpi@gmail.com>
2024-08-30 10:30:55 -0500

    Implementacion incial del dashboard

--- src/app/Dashboard/components/Layout.tsx
+++ src/app/Dashboard/components/Layout.tsx
@@ -1,26 +1,14 @@
 import * as React from 'react';
-import { styled} from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiDrawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
-import MenuIcon from '@mui/icons-material/Menu';
-import LogoutIcon from '@mui/icons-material/Logout';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import { mainListItems, secondaryListItems } from './MenuItem';
-import { Outlet, useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { clearUser } from '../../../state/slices/authSlice';
+import GlobalStyles from '@mui/material/GlobalStyles';
+
+import { MainNav } from './main-nav';
+import { SideNav } from './side-nav';
+import { layoutConfig } from '../config';
+
+
 
 function Copyright(props: any) {
      return (
@@ -35,154 +23,155 @@ function Copyright(props: any) {
      );
 }
 
-const drawerWidth: number = 240;
-
-interface AppBarProps extends MuiAppBarProps {
-     open?: boolean;
+export interface VerticalLayoutProps {
+     children?: React.ReactNode;
 }
 
-const AppBar = styled(MuiAppBar, {
-     shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-     zIndex: theme.zIndex.drawer + 1,
-     transition: theme.transitions.create(['width', 'margin'], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-     }),
-     ...(open && {
-          marginLeft: drawerWidth,
-          width: `calc(100% - ${drawerWidth}px)`,
-          transition: theme.transitions.create(['width', 'margin'], {
-               easing: theme.transitions.easing.sharp,
-               duration: theme.transitions.duration.enteringScreen,
-          }),
-     }),
-}));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-     ({ theme, open }) => ({
-          '& .MuiDrawer-paper': {
-               position: 'relative',
-               whiteSpace: 'nowrap',
-               width: drawerWidth,
-               transition: theme.transitions.create('width', {
-                    easing: theme.transitions.easing.sharp,
-                    duration: theme.transitions.duration.enteringScreen,
-               }),
-               boxSizing: 'border-box',
-               ...(!open && {
-                    overflowX: 'hidden',
-                    transition: theme.transitions.create('width', {
-                         easing: theme.transitions.easing.sharp,
-                         duration: theme.transitions.duration.leavingScreen,
-                    }),
-                    width: theme.spacing(7),
-                    [theme.breakpoints.up('sm')]: {
-                         width: theme.spacing(9),
-                    },
-               }),
-          },
-     }),
-);
-
-
 export default function Layout() {
-
-     const dispatch = useDispatch();
-     const navigate = useNavigate();
-
-     const [open, setOpen] = React.useState(true);
-     const toggleDrawer = () => {
-          setOpen(!open);
-     };
-
-     const logOut = () =>{
-          dispatch(clearUser())
-          navigate('/auth/login')
-     }
-
-
      return (
-          <Box sx={{ display: 'flex' }}>
-               <CssBaseline />
-               <AppBar position="absolute" open={open}>
-                    <Toolbar
-                         sx={{
-                              pr: '24px', // keep right padding when drawer closed
-                         }}
-                    >
-                         <IconButton
-                              edge="start"
-                              color="inherit"
-                              aria-label="open drawer"
-                              onClick={toggleDrawer}
-                              sx={{
-                                   marginRight: '36px',
-                                   ...(open && { display: 'none' }),
-                              }}
-                         >
-                              <MenuIcon />
-                         </IconButton>
-                         <Typography
-                              component="h1"
-                              variant="h6"
-                              color="inherit"
-                              noWrap
-                              sx={{ flexGrow: 1 }}
-                         >
-                              Dashboard
-                         </Typography>
-                         <IconButton color="inherit">
-                              <Badge badgeContent={4} color="secondary">
-                                   <NotificationsIcon />
-                              </Badge>
-                              <LogoutIcon 
-                                   sx={{ marginLeft: 3 }}
-                                   onClick={logOut}
-                              />
-                         </IconButton>
-                    </Toolbar>
-               </AppBar>
-               <Drawer variant="permanent" open={open}>
-                    <Toolbar
-                         sx={{
-                              display: 'flex',
-                              alignItems: 'center',
-                              justifyContent: 'flex-end',
-                              px: [1],
-                         }}
-                    >
-                         <IconButton onClick={toggleDrawer}>
-                              <ChevronLeftIcon />
-                         </IconButton>
-                    </Toolbar>
-                    <Divider />
-                    <List component="nav">
-                         {mainListItems}
-                         <Divider sx={{ my: 1 }} />
-                         {secondaryListItems}
-                    </List>
-               </Drawer>
+          <React.Fragment>
+               <GlobalStyles
+                    styles={{
+                         body: {
+                              '--MainNav-height': '56px',
+                              '--MainNav-zIndex': 1000,
+                              '--SideNav-width': '280px',
+                              '--SideNav-zIndex': 1100,
+                              '--MobileNav-width': '320px',
+                              '--MobileNav-zIndex': 1100,
+                         },
+                    }}
+               />
                <Box
-                    component="main"
                     sx={{
-                         backgroundColor: (theme) =>
-                              theme.palette.mode === 'light'
-                                   ? theme.palette.grey[100]
-                                   : theme.palette.grey[900],
-                         flexGrow: 1,
-                         height: '100vh',
-                         overflow: 'auto',
+                         bgcolor: 'var(--mui-palette-background-default)',
+                         display: 'flex',
+                         flexDirection: 'column',
+                         position: 'relative',
+                         minHeight: '100%',
                     }}
                >
-                    <Toolbar />
-                    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-                         <Grid container spacing={3}>
-                              <Outlet />
-                         </Grid>
-                         <Copyright sx={{ pt: 4 }} />
-                    </Container>
+                    {/* <SideNav color={settings.navColor} items={layoutConfig.navItems} /> */}
+                    <SideNav  items={layoutConfig.navItems} />
+                    <Box sx={{ display: 'flex', flex: '1 1 auto', flexDirection: 'column', pl: { lg: 'var(--SideNav-width)' } }}>
+                         <MainNav items={layoutConfig.navItems} />
+                         <Box
+                              component="main"
+                              sx={{
+                                   '--Content-margin': '0 auto',
+                                   '--Content-maxWidth': 'var(--maxWidth-xl)',
+                                   '--Content-paddingX': '24px',
+                                   '--Content-paddingY': { xs: '24px', lg: '64px' },
+                                   '--Content-padding': 'var(--Content-paddingY) var(--Content-paddingX)',
+                                   '--Content-width': '100%',
+                                   display: 'flex',
+                                   flex: '1 1 auto',
+                                   flexDirection: 'column',
+                              }}
+                         >
+                              {/* {children} */}
+                         </Box>
+                    </Box>
                </Box>
-          </Box>
+          </React.Fragment>
      );
+     //////////////////////////////////////////////////////////
+
+     // const dispatch = useDispatch();
+     // const navigate = useNavigate();
+
+     // const [open, setOpen] = React.useState(true);
+     // const toggleDrawer = () => {
+     //      setOpen(!open);
+     // };
+
+     // const logOut = () =>{
+     //      dispatch(clearUser())
+     //      navigate('/auth/login')
+     // }
+
+     // return (
+
+     //      // <Box sx={{ display: 'flex' }}>
+     //      //      <CssBaseline />
+     //      //      <AppBar position="absolute" open={open}>
+     //      //           <Toolbar
+     //      //                sx={{
+     //      //                     pr: '24px', // keep right padding when drawer closed
+     //      //                }}
+     //      //           >
+     //      //                <IconButton
+     //      //                     edge="start"
+     //      //                     color="inherit"
+     //      //                     aria-label="open drawer"
+     //      //                     onClick={toggleDrawer}
+     //      //                     sx={{
+     //      //                          marginRight: '36px',
+     //      //                          ...(open && { display: 'none' }),
+     //      //                     }}
+     //      //                >
+     //      //                     <MenuIcon />
+     //      //                </IconButton>
+     //      //                <Typography
+     //      //                     component="h1"
+     //      //                     variant="h6"
+     //      //                     color="inherit"
+     //      //                     noWrap
+     //      //                     sx={{ flexGrow: 1 }}
+     //      //                >
+     //      //                     Dashboard
+     //      //                </Typography>
+     //      //                <IconButton color="inherit">
+     //      //                     <Badge badgeContent={4} color="secondary">
+     //      //                          <NotificationsIcon />
+     //      //                     </Badge>
+     //      //                     <LogoutIcon 
+     //      //                          sx={{ marginLeft: 3 }}
+     //      //                          onClick={logOut}
+     //      //                     />
+     //      //                </IconButton>
+     //      //           </Toolbar>
+     //      //      </AppBar>
+     //      //      <Drawer variant="permanent" open={open}>
+     //      //           <Toolbar
+     //      //                sx={{
+     //      //                     display: 'flex',
+     //      //                     alignItems: 'center',
+     //      //                     justifyContent: 'flex-end',
+     //      //                     px: [1],
+     //      //                }}
+     //      //           >
+     //      //                <IconButton onClick={toggleDrawer}>
+     //      //                     <ChevronLeftIcon />
+     //      //                </IconButton>
+     //      //           </Toolbar>
+     //      //           <Divider />
+     //      //           <List component="nav">
+     //      //                {mainListItems}
+     //      //                <Divider sx={{ my: 1 }} />
+     //      //                {secondaryListItems}
+     //      //           </List>
+     //      //      </Drawer>
+     //      //      <Box
+     //      //           component="main"
+     //      //           sx={{
+     //      //                backgroundColor: (theme) =>
+     //      //                     theme.palette.mode === 'light'
+     //      //                          ? theme.palette.grey[100]
+     //      //                          : theme.palette.grey[900],
+     //      //                flexGrow: 1,
+     //      //                height: '100vh',
+     //      //                overflow: 'auto',
+     //      //           }}
+     //      //      >
+     //      //           <Toolbar />
+     //      //           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+     //      //                <Grid container spacing={3}>
+     //      //                     <Outlet />
+     //      //                </Grid>
+     //      //                <Copyright sx={{ pt: 4 }} />
+     //      //           </Container>
+     //      //      </Box>
+     //      // </Box>
+     // );
 }
--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -46,7 +46,6 @@ export default function LoginPage() {
                localStorage.setItem("token", response.data.tokens);
                navigate('/');
                
-
                //
 
           } catch (error) {
